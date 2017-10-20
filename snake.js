259c9e8c4b0449dcc1e4949df2dfdb55d9005133
nandukalidindi <nandukalidindi@gmail.com>
2017-10-20 02:20:54 -0400

    Add snake class and raphael minified JS to build snake

---
+++ snake.js
@@ -0,0 +1,50 @@
+'use strict';
+
+const paper = Raphael(0, 0, window.innerWidth, window.innerHeight);
+
+const anotherPaper = Raphael(0, 0, window.innerWidth, window.innerHeight);
+
+class Position {
+	constructor(x, y) {
+		Object.assign(this, { x, y });
+	}
+}
+
+class Snake {
+	constructor(skeleton = "rectangles", color = "black", head = { x: 50, y: 10 }) {
+		Object.assign(this, { 
+				skeleton, color, 
+				head, 
+				body: [...Array(4)].map((iter, i) => ({ x: (head.x - i*20), y: head.y })) 
+			}
+		);
+	}
+
+	segment(x, y) {
+		return { x, y };
+	}
+
+	moveMap() {
+		return {
+			"up": () => { this.head = { x: this.head.x, y: this.head.y - 20 } },
+			"down": () => { this.head = { x: this.head.x, y: this.head.y + 20 } },
+			"right": () => { this.head = { x: this.head.x + 20, y: this.head.y } },
+			"left": () => { this.head = { x: this.head.x - 20, y: this.head.y } },
+		}
+	}
+
+	eatAndGrow() {
+	}
+
+	renderBody(direction, ate) {
+		ate && this.body.splice(-1);
+		this.moveMap()[direction]();		
+
+		this.body = [this.head, ...this.body];
+		this.body.forEach(part => {
+			let circle = paper.rect(part.x, part.y, 20, 20);
+			circle.attr("fill", "#f00");
+			circle.attr("stroke", "#fff");
+		});
+	}
+}
